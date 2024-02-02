dff095ed6d6c84dee0877b0c1757c9cdf6364c1f
Coridyn Fitzgerald-Hood <coridyn@gmail.com>
2024-02-02 20:55:45 +1000

    refactor(lib): remove 'promised-io', 'colors', 'request' dependencies'; remove unused devDependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,39 +1,17 @@
 module.exports = function(grunt) {
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+  // grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  // grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Project configuration.
   grunt.initConfig({
-    nodeunit: {
-      files: ['test/**/*.js']
-    },
-    watch: {
-      files: '<%= jshint.files %>',
-      tasks: 'default'
-    },
-    jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        boss: true,
-        eqnull: true,
-        node: true,
-        globals: {}
-      },
-      files: [
-        'Gruntfile.js',
-        'tasks/**/*.js',
-        'test/**/*.js'
-      ]
-    },
+    // nodeunit: {
+    //   files: ['test/**/*.js']
+    // },
+    // watch: {
+    //   files: '<%= jshint.files %>',
+    //   tasks: 'default'
+    // },
     modernizr: {
       dist: {}
     }
@@ -41,11 +19,5 @@ module.exports = function(grunt) {
 
   // Load local tasks.
   grunt.loadTasks('tasks');
-
-  // Default task.
-  grunt.registerTask('default', [
-    'jshint'//,
-    //'nodeunit' // there is no test for now, nodeunit task break if there isn't
-  ]);
 
 };
